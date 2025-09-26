82af3278cfc659bbfb65d44d4e020b649e8c1c24
Tomas Zingis <thebash2344@gmail.com>
2025-09-26 12:49:24 +0200

    seo + ssr

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,13 +3,37 @@ import Header from "@/componets/Header/Header";
 import Hero from "@/componets/Hero/Hero";
 import Tarifs from "@/componets/Tarifs/Tarifs";
 import { routing } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
 
-export async function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations("HomePage");
+  const BASE_URL =
+    "https://cyan-dev-test-task-e3j18z6uh-tomas-zingis-projects.vercel.app";
+  const currentPath = "/";
+  const alternates = routing.locales.map((lang: string) => ({
+    hrefLang: lang,
+    href: `${BASE_URL}/${lang}${currentPath}`,
+  }));
+  alternates.push({
+    hrefLang: "x-default",
+    href: `${BASE_URL}/${routing.defaultLocale}${currentPath}`,
+  });
+
+  return {
+    title: t("seo_title"),
+    description: t("seo_description"),
+    alternates: {
+      canonical: `${BASE_URL}/${locale}${currentPath}`,
+      languages: alternates,
+    },
+  };
 }
 
 const HomePage = async () => {
-  // const t = await getTranslations("HomePage");
   return (
     <div className="flex flex-col bg-[#0c0117]">
       <Header />
